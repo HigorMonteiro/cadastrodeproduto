16eac036f79f638d9df4806c8b07315d606e31e8
higor <higor.tecinfor@gmail.com>
2016-10-25 17:55:17 -0300

    corrigindo name da url

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url, include
 from . import views
 from core.views import ProductListView, ProductCreateView, ProductUpdateView,\
-ProductDetailView, ProductDeleteView, AutoComplete
+ProductDetailView, ProductDeleteView, AutoComplete, HomeView
 
 
 urlpatterns = [
-    url(r'^$', ProductListView.as_view(), name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^list/$', ProductListView.as_view(), name='product_list'),
     url(r'^add/$', ProductCreateView.as_view(), name='product_add'),
     url(r'^product/(?P<pk>\d+)/$', ProductDetailView.as_view(), name='product_detail'),
     url(r'^product/editar/(?P<pk>\d+)/$', ProductUpdateView.as_view(), name='product_update'),
