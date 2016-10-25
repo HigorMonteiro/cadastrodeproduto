00f46a8314756c81b93d1d34f84fdcf2ae07e427
higor <higor.tecinfor@gmail.com>
2016-10-25 14:11:21 -0300

    criando compo de busca com autocomplate

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from . import views
-from core.views import ProductListView, ProductCreateView, ProductUpdateView, ProductDetailView, ProductDeleteView
+from core.views import ProductListView, ProductCreateView, ProductUpdateView,\
+ProductDetailView, ProductDeleteView, AutoComplete
 
 
 urlpatterns = [
@@ -8,6 +9,7 @@ urlpatterns = [
     url(r'^add/$', ProductCreateView.as_view(), name='product_add'),
     url(r'^product/(?P<pk>\d+)/$', ProductDetailView.as_view(), name='product_detail'),
     url(r'^product/editar/(?P<pk>\d+)/$', ProductUpdateView.as_view(), name='product_update'),
-     url(r'^product/delete/(?P<pk>\d+)/$', ProductDeleteView.as_view(), name='product_delete'),
+    url(r'^product/delete/(?P<pk>\d+)/$', ProductDeleteView.as_view(), name='product_delete'),
+    url(r'autocomplete/$',AutoComplete.as_view(), name='product-auto'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.core.urlresolvers import reverse_lazy
+from django.db.models import Q
 from django.conf import settings
 from django.views import generic
 from .models import Product
+import json
 
 
 class ProductListView(generic.ListView):
@@ -32,3 +35,14 @@ class ProductUpdateView(generic.UpdateView):
 class ProductDeleteView(generic.DeleteView):
     model = Product
     success_url = reverse_lazy('core:home')
+
+class AutoComplete(generic.View):
+    def get(self, request, *args, **kwargs):
+        query = Q()
+        if request.GET.get('term', False):
+            query = Q(name__icontains=request.GET['term'])
+        products = Product.objects.filter(query)
+        dados = [{'id': product.id, 'name': product.product} for product in products]
+        mimetype = "application/json;charset=UTF-8"
+        js = json.dumps(dados, ensure_ascii=False).encode('utf8')
+        return HttpResponse(js, mimetype)
