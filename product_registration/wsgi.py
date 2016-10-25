e52514b75af615b69641bc7c2e8bb0678f348661
higor <higor.tecinfor@gmail.com>
2016-10-25 09:15:24 -0300

    implementando Whitenoise

--- product_registration/wsgi.py
+++ product_registration/wsgi.py
@@ -6,6 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 import os
 
@@ -14,3 +16,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "product_registration.settings")
 
 application = get_wsgi_application()
+
+application = DjangoWhiteNoise(application)
