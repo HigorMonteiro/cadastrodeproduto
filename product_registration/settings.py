3d24fc625086bd6e23602582b2c4e51ae6ec984c
higor <higor.tecinfor@gmail.com>
2016-10-25 04:57:25 -0300

    mundo estado do Debug

--- product_registration/settings.py
+++ product_registration/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = '_u-k74@x9qzl4oy9qp5#8bmbl3+9ayg3hkp!+^&6s9o*=6fnrk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
